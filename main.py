f7ee795cebad97a8039a07ffee7821c6bd499dcd
Benjamin <bhillenbrand101@gmail.com>
2018-09-12 18:33:19 -0500

    Game will continue till player wants to quit, "n" for quit

--- main.py
+++ main.py
@@ -21,55 +21,61 @@ except ImportError:
     print("No GUI option")
 
 
+play_again = True
+while play_again == True:
+
+    # used to hold list of players cards
+    arrayPlayer1 = []
+    arrayPlayer2 = []
+    arrayPlayer3 = []
+    arrayPlayer4 = []
+
+    # used to hold list of player cards value
+    arrayPlayer1Numbers = []
+    arrayPlayer2Numbers = []
+    arrayPlayer3Numbers = []
+    arrayPlayer4Numbers = []
+
+    for x in range(3):
+        card = CardDeck.draw_cards()
+        arrayPlayer1.append(card[0])
+        arrayPlayer1Numbers.append(card[1])
+        #print(arrayPlayer1Numbers)   # used for data testing
+        #print(total_score(arrayPlayer1Numbers))
+        card2 = CardDeck.draw_cards()
+        arrayPlayer2.append(card2[0])
+        arrayPlayer2Numbers.append(card2[1])
+        card3 = CardDeck.draw_cards()
+        arrayPlayer3.append(card3[0])
+        arrayPlayer3Numbers.append(card3[1])
+        card4 = CardDeck.draw_cards()
+        arrayPlayer4.append(card4[0])
+        arrayPlayer4Numbers.append(card4[1])
+
+    # this list is used to determine whom wins the match
+    playerListNumbers = [FindWinner.total_score(arrayPlayer1Numbers), FindWinner.total_score(arrayPlayer2Numbers),
+                         FindWinner.total_score(arrayPlayer3Numbers), FindWinner.total_score(arrayPlayer4Numbers)]
+
+
+    print("Your cards are: ", arrayPlayer1, "Your score is: ", FindWinner.total_score(arrayPlayer1Numbers))  # not sure which looks better for viewing change how you wish lucky
+    print("Player two cards: ", arrayPlayer2, "score is: ", FindWinner.total_score(arrayPlayer2Numbers))
+    print("Player three cards: ")
+    print(arrayPlayer3, "score is: ", FindWinner.total_score(arrayPlayer3Numbers))
+    print("Player four cards: ")
+    print(arrayPlayer4, "score is: ", FindWinner.total_score(arrayPlayer4Numbers))
+    FindWinner.compare_score(playerListNumbers)  # Will call the method to find the winner
+
+    round_two = input("Play again? Enter for yes, n for no: ")  # to get user input for if they want to quit
+    if round_two == "n":  # seeing if it matches user input
+        sys.exit(0)  # exiting the game
+    #elif round_two == "y":  # not needed Enter Button will play another game
 
 
 
-# used to hold list of players cards
-arrayPlayer1 = []
-arrayPlayer2 = []
-arrayPlayer3 = []
-arrayPlayer4 = []
-
-# used to hold list of player cards value
-arrayPlayer1Numbers = []
-arrayPlayer2Numbers = []
-arrayPlayer3Numbers = []
-arrayPlayer4Numbers = []
-
-for x in range(3):
-    card = CardDeck.draw_cards()
-    arrayPlayer1.append(card[0])
-    arrayPlayer1Numbers.append(card[1])
-    #print(arrayPlayer1Numbers)   # used for data testing
-    #print(total_score(arrayPlayer1Numbers))
-    card2 = CardDeck.draw_cards()
-    arrayPlayer2.append(card2[0])
-    arrayPlayer2Numbers.append(card2[1])
-    card3 = CardDeck.draw_cards()
-    arrayPlayer3.append(card3[0])
-    arrayPlayer3Numbers.append(card3[1])
-    card4 = CardDeck.draw_cards()
-    arrayPlayer4.append(card4[0])
-    arrayPlayer4Numbers.append(card4[1])
-
-# this list is used to determine whom wins the match
-playerListNumbers = [FindWinner.total_score(arrayPlayer1Numbers), FindWinner.total_score(arrayPlayer2Numbers),
-                     FindWinner.total_score(arrayPlayer3Numbers), FindWinner.total_score(arrayPlayer4Numbers)]
-
-
+# billyBob = PlayerClass.Player("bob")
 
 #Player = bob()
 #bob = Player()
 #bob.play()
 #bob.play_winner()
 
-print("Your cards are: ", arrayPlayer1, "Your score is: ", FindWinner.total_score(arrayPlayer1Numbers))  # not sure which looks better for viewing change how you wish lucky
-print("Player two cards: ", arrayPlayer2, "score is: ", FindWinner.total_score(arrayPlayer2Numbers))
-print("Player three cards: ")
-print(arrayPlayer3, "score is: ", FindWinner.total_score(arrayPlayer3Numbers))
-print("Player four cards: ")
-print(arrayPlayer4, "score is: ", FindWinner.total_score(arrayPlayer4Numbers))
-FindWinner.compare_score(playerListNumbers)  # Will call the method to find the winner
-
-
-# billyBob = PlayerClass.Player("bob")
